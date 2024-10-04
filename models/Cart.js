2b5ef1d19ddbccc6e44201272d74e86360b92a0a
bnvenky <bnvenkatesheee@gmail.com>
2024-10-04 14:16:00 +0530

    update cart route  commit

--- models/Cart.js
+++ models/Cart.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
 });
 
 const Cart = mongoose.model('Cart', cartSchema);
